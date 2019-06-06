9b3f0b768e5aeb943c90908d953f6d8e9ad72917
davebeauchemin <37642902+davebeauchemin@users.noreply.github.com>
2019-06-06 15:50:37 -0400

    Updated

--- sick-fits/frontend/components/Signup.js
+++ sick-fits/frontend/components/Signup.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Query, Mutation } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
 
 const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION(
@@ -35,48 +36,52 @@ class Signup extends Component {
 
   render() {
     return (
-      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
-        {(signup, { error, loading }) => {
-          return (
-            <Form method="post" onSubmit={async e => {
-              e.preventDefault();
-              await signup();
-              this.setState({ name: '', email: '', password: '' })
-            }}>
-              <fieldset disabled={loading} aria-busy={loading}>
-                <h2>Sign Up for an Account</h2>
-                <Error error={error} />
-                <label htmlFor="name">
-                  Name
+      <Mutation
+        mutation={SIGNUP_MUTATION}
+        variables={this.state}
+        refetchQueries={[
+          { query: CURRENT_USER_QUERY }
+        ]}
+      >
+        {(signup, { error, loading }) => (
+          <Form method="post" onSubmit={async e => {
+            e.preventDefault();
+            await signup();
+            this.setState({ name: '', email: '', password: '' })
+          }}>
+            <fieldset disabled={loading} aria-busy={loading}>
+              <h2>Sign Up for an Account</h2>
+              <Error error={error} />
+              <label htmlFor="name">
+                Name
                 <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    value={this.state.name}
-                    onChange={this.saveToState} />
-                </label>
-                <label htmlFor="email">
-                  Email
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.saveToState} />
+              </label>
+              <label htmlFor="email">
+                Email
                 <input
-                    type="email"
-                    name="email"
-                    placeholder="Email" value={this.state.email}
-                    onChange={this.saveToState} />
-                </label>
-                <label htmlFor="password">
-                  Password
+                  type="email"
+                  name="email"
+                  placeholder="Email" value={this.state.email}
+                  onChange={this.saveToState} />
+              </label>
+              <label htmlFor="password">
+                Password
                 <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.saveToState} />
-                </label>
-                <button type="submit">Sign Up</button>
-              </fieldset>
-            </Form>
-          )
-        }}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.saveToState} />
+              </label>
+              <button type="submit">Sign Up</button>
+            </fieldset>
+          </Form>
+        )}
       </Mutation>
     )
   }
